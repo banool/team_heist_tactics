938583f366299e74c23a3da9a2b3cddc3d28521b
Daniel Porteous <danielporteous1@gmail.com>
2020-06-16 08:28:19 -0700

    Fix load_map tests

--- src/load_map.rs
+++ src/load_map.rs
@@ -1,6 +1,7 @@
 // Load data from the data/ directory into the game
 
 use crate::types::{SerializableTile, Tile};
+
 use std::collections::HashMap;
 
 pub fn load_serializable_tiles_from_json() -> HashMap<String, SerializableTile> {
@@ -18,27 +19,33 @@ pub fn load_tiles_from_json() -> HashMap<String, Tile> {
     tile_map
 }
 
-#[test]
-pub fn serialize_file_test() -> () {
-    // for discovering how to write these
-    let mut my_squares: Vec<Square> = Vec::new();
-    for _ in 0..16 {
-        let sq = Square {
-            north_wall: WallType::Clear,
-            east_wall: WallType::Clear,
-            south_wall: WallType::Clear,
-            west_wall: WallType::Clear,
-            square_type: SquareType::Normal,
-        };
-        my_squares.push(sq);
-    }
-    let my_pos = MapPosition { x: 0, y: 0 };
-    let tile = Tile {
-        squares: my_squares,
-        position: my_pos,
-    };
+#[allow(dead_code, unused_imports)]
+mod tests {
+    use log::info;
+    use crate::types::{SerializableTile, Tile, MapPosition, Square, WallType, SquareType};
 
-    let st: SerializableTile = SerializableTile::from(tile);
-    let serialized = serde_json::to_string(&st).unwrap();
-    info!("{}", serialized);
+    #[test]
+    pub fn serialize_file_test() -> () {
+        // for discovering how to write these
+        let mut my_squares: Vec<Square> = Vec::new();
+        for _ in 0..16 {
+            let sq = Square {
+                north_wall: WallType::Clear,
+                east_wall: WallType::Clear,
+                south_wall: WallType::Clear,
+                west_wall: WallType::Clear,
+                square_type: SquareType::Normal,
+            };
+            my_squares.push(sq);
+        }
+        let my_pos = MapPosition { x: 0, y: 0 };
+        let tile = Tile {
+            squares: my_squares,
+            position: my_pos,
+        };
+
+        let st: SerializableTile = SerializableTile::from(tile);
+        let serialized = serde_json::to_string(&st).unwrap();
+        info!("{}", serialized);
+    }
 }
