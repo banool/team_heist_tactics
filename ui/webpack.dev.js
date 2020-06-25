8005b4bacebdbc4de514ca4adf5850bce78290b0
Daniel Porteous <danielporteous1@gmail.com>
2020-06-25 09:54:41 -0700

    Upgrade or remove more UI packages

--- ui/webpack.dev.js
+++ ui/webpack.dev.js
@@ -4,13 +4,5 @@ const path = require("path");
 
 module.exports = merge(common, {
   devtool: "eval-source-map",
-  devServer: {
-    index: "dist/index.html",
-    serveIndex: true,
-    host: '0.0.0.0',
-    port: 8080,
-    hot: true,
-    historyApiFallback: true
-  },
   mode: "development"
 });
