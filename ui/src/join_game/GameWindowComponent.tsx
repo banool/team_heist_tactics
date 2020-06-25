31829189371835307e8a318149edf824dca13708
Daniel Porteous <danielporteous1@gmail.com>
2020-06-25 16:24:11 -0700

    Add shadows to tiles by drawing a square with a shadow under each tile

--- ui/src/join_game/GameWindowComponent.tsx
+++ ui/src/join_game/GameWindowComponent.tsx
@@ -30,37 +30,100 @@ import {
 } from "./overlay_components";
 import styles from "../components/styles";
 import ConnectionStatusComponent from "./ConnectionStatusComponent";
+import { CanvasPosition } from "./types";
 
-type TileProps = {
+type GetTileShadowSquareProps = {
   proto_tile: ProtoTile;
+  width: number;
+  height: number;
 };
-// The offset makes the center of the image be the center of the canvas element.
-const Tile = ({ proto_tile }: TileProps) => {
-  // TODO Consider preloading the next / all images.
-  // Probably not necessary becayse images are cached in the client, so the user
-  // only ever suffers the slow load time once.
-  const url = `static/images/00${proto_tile.getName()}.jpg`;
-  const [image, status] = useImage(url);
 
+const get_tile_and_shadow_tile = ({
+  proto_tile,
+  width,
+  height,
+}: GetTileShadowSquareProps) => {
   const size = TILE_SIZE;
   const offset = size / 2;
   const pixel_offset = -INTERNAL_TILE_OFFSET;
 
   var map_position = proto_tile.getPosition()!;
-  var { width, height } = useWindowDimensions();
-  var canvas_position = mapPositionToCanvasPosition(map_position, pixel_offset, 1, 3, width, height);
-
+  var canvas_position = mapPositionToCanvasPosition(
+    map_position,
+    pixel_offset,
+    1,
+    3,
+    width,
+    height
+  );
   var num_rotations = proto_tile.getNumRotations();
+  var tile_name = proto_tile.getName();
 
   console.log(
-    `fuck tile at canvas.x/y ${canvas_position.x} ${canvas_position.y} map ${map_position} rotated ${num_rotations} times`
+    `fuck tile at canvas.x/y ${canvas_position.x} ${
+      canvas_position.y
+    } map ${map_position.getX()} ${map_position.getY()} rotated ${num_rotations} times`
   );
+
+  var t = (
+    <Tile
+      key={tile_name}
+      tile_name={tile_name}
+      size={size}
+      num_rotations={num_rotations}
+      offset={offset}
+      canvas_position={canvas_position}
+    />
+  );
+
+  var st = (
+    <Rect
+      key={`${tile_name}_square`}
+      x={canvas_position.x+5}
+      y={canvas_position.y+5}
+      width={TILE_SIZE-10}
+      height={TILE_SIZE-10}
+      shadowBlur={20}
+      shadowColor="black"
+      shadowEnabled={true}
+      stroke="black"
+      strokeWidth={10}
+      offsetX={TILE_SIZE / 2}
+      offsetY={TILE_SIZE / 2}
+    />
+  );
+
+  return { tile: t, shadow_tile: st };
+};
+
+type TileProps = {
+  tile_name: string;
+  size: number;
+  num_rotations: number;
+  offset: number;
+  canvas_position: CanvasPosition;
+};
+// The offset makes the center of the image be the center of the canvas element.
+const Tile = ({
+  tile_name,
+  size,
+  num_rotations,
+  offset,
+  canvas_position,
+}: TileProps) => {
+  // TODO Consider preloading the next / all images.
+  // Probably not necessary becayse images are cached in the client, so the user
+  // only ever suffers the slow load time once.
+  console.log("tilename", tile_name);
+  const url = `static/images/00${tile_name}.jpg`;
+  console.log("url", url);
+  const [image, status] = useImage(url);
 
   var comp: JSX.Element;
   if (status === "loaded") {
     comp = (
       <Image
-        //shadowBlur={10}
+        //shadowBlur={15}
         image={image}
         width={size}
         height={size}
@@ -104,7 +167,8 @@ const Heister = ({ proto_heister }: HeisterProps) => {
     pixel_offset,
     tile_offset_x,
     tile_offset_y,
-    width, height
+    width,
+    height
   );
 
   console.log(
@@ -204,8 +268,8 @@ const PossiblePlacement = ({ map_position }: PossiblePlacementProps) => {
       height={INTERNAL_SQUARE_SIZE}
       stroke="black"
       strokeWidth={stroke_width}
-      offsetX={INTERNAL_SQUARE_SIZE/4}
-      offsetY={INTERNAL_SQUARE_SIZE/4}
+      offsetX={INTERNAL_SQUARE_SIZE / 4}
+      offsetY={INTERNAL_SQUARE_SIZE / 4}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
       onClick={onClick}
@@ -214,13 +278,18 @@ const PossiblePlacement = ({ map_position }: PossiblePlacementProps) => {
       shadowColor="black"
       shadowEnabled={shadowEnabled}
     />
-  )
-}
+  );
+};
 
 // This uses special <> syntax to return multiple components.
 const Tiles = ({ tiles }) => <>{tiles.map((t: any) => t)}</>;
 const Heisters = ({ heisters }) => <>{heisters.map((h: any) => h)}</>;
-const PossiblePlacements = ({ possible_placements }) => <>{possible_placements.map((p: any) => p)}</>;
+const PossiblePlacements = ({ possible_placements }) => (
+  <>{possible_placements.map((p: any) => p)}</>
+);
+const ShadowTiles = ({ shadow_tiles }) => (
+  <>{shadow_tiles.map((st: any) => st)}</>
+);
 
 const GameWindowComponent = () => {
   const game_state = useSelector(gameStateSelector);
@@ -230,19 +299,24 @@ const GameWindowComponent = () => {
   // TODO: Make one of these per heister, to reduce necessary updates.
   useSelector(numInvalidMoveAttemptsSelector);
 
+  var { width, height } = useWindowDimensions();
+
   // https://reactjs.org/docs/lists-and-keys.html#keys
   const getTiles = () => {
     var proto_tiles = game_state!.getTilesList();
     var tiles: JSX.Element[] = [];
+    var shadow_tiles: JSX.Element[] = [];
     for (let i = 0; i < proto_tiles.length; i++) {
-      var t = (
-        <Provider key={i} store={store}>
-          <Tile key={i + 100} proto_tile={proto_tiles[i]} />
-        </Provider>
-      );
-      tiles.push(t);
+      var proto_tile = proto_tiles[i];
+      var { tile, shadow_tile } = get_tile_and_shadow_tile({
+        proto_tile: proto_tile,
+        width: width,
+        height: height,
+      });
+      tiles.push(tile);
+      shadow_tiles.push(shadow_tile);
     }
-    return tiles;
+    return { tiles: tiles, shadow_tiles: shadow_tiles };
   };
 
   const getHeisters = () => {
@@ -271,13 +345,16 @@ const GameWindowComponent = () => {
     for (let i = 0; i < proto_possible_placements.length; i++) {
       var pp = (
         <Provider key={i} store={store}>
-          <PossiblePlacement key={i + 300} map_position={proto_possible_placements[i]} />
+          <PossiblePlacement
+            key={i + 300}
+            map_position={proto_possible_placements[i]}
+          />
         </Provider>
       );
       possiblePlacements.push(pp);
     }
     return possiblePlacements;
-  }
+  };
 
   const [stageX, setStageX] = useState(0);
   const [stageY, setStageY] = useState(0);
@@ -294,7 +371,7 @@ const GameWindowComponent = () => {
   const GREEN_HEISTER_KEYBOARD_ICON = 100;
   const ORANGE_HEISTER_KEYBOARD_ICON = 135;
 
-  var { width, height } = useWindowDimensions();
+  var { tiles, shadow_tiles } = getTiles();
 
   // <div style={{ width: "90%", transform: "translate(+5%, 0%)", backgroundColor: "#ffffff" }}>
 
@@ -318,7 +395,8 @@ const GameWindowComponent = () => {
           transformsEnabled={"position"}
         >
           <Layer>
-            <Tiles tiles={getTiles()} />
+            <ShadowTiles shadow_tiles={shadow_tiles} />
+            <Tiles tiles={tiles} />
             <Heisters heisters={getHeisters()} />
             <PossiblePlacements possible_placements={getPossiblePlacements()} />
           </Layer>
