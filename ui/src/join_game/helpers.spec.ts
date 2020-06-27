e6838131f82f1e0eaa953d2716ca7861d476e7ca
Kelly McBride <kemcbride28@gmail.com>
2020-06-26 18:18:28 -0700

    Update unit tests and functions for mapToCanvasPos + canvasToMapPos

--- ui/src/join_game/helpers.spec.ts
+++ ui/src/join_game/helpers.spec.ts
@@ -3,17 +3,84 @@ import {
   mapPositionToCanvasPosition,
 } from "./helpers";
 import { MapPosition } from "../generated/types_pb";
+import { INTERNAL_SQUARE_SIZE, INTERNAL_TILE_OFFSET } from "../constants/other";
+let CANVAS_HEIGHT = 1000;
+let CANVAS_WIDTH = 1600;
 
 test("map_position translation both directions for 0,0 should return 0,0", () => {
   let pixel_offset = 2;
   let center = new MapPosition();
   center.setX(0);
   center.setY(0);
-  let a = mapPositionToCanvasPosition(center, pixel_offset, 0, 0);
-  let b = canvasPositionToMapPosition(a, pixel_offset);
+  let a = mapPositionToCanvasPosition(center, pixel_offset, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let b = canvasPositionToMapPosition(a, pixel_offset, CANVAS_WIDTH, CANVAS_HEIGHT);
   expect(b.getX()).toBe(center.getX());
   expect(b.getY()).toBe(center.getY());
 });
+
+test("tile 0,0 map_position reversible translation", () => {
+  let pixel_offset = 0;
+  let center = new MapPosition();
+  center.setX(0);
+  center.setY(0);
+  let a = mapPositionToCanvasPosition(center, pixel_offset, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let b = canvasPositionToMapPosition(a, pixel_offset, CANVAS_WIDTH, CANVAS_HEIGHT);
+  expect(a.x).toBe(CANVAS_WIDTH / 2);
+  expect(a.y).toBe(CANVAS_HEIGHT / 2);
+  expect(b.getX()).toBe(center.getX());
+  expect(b.getY()).toBe(center.getY());
+});
+
+test("tile 1,-4 map_position reversible translation", () => {
+  let pixel_offset = 0;
+  let tile_corner = new MapPosition();
+  tile_corner.setX(1);
+  tile_corner.setY(-4);
+  let a = mapPositionToCanvasPosition(tile_corner, pixel_offset, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // For this simple case, I can guess what the resulting canvas value is. But in general, I don't want to test that.
+  expect(a.x).toBe(CANVAS_WIDTH / 2 + INTERNAL_SQUARE_SIZE);
+  expect(a.y).toBe(CANVAS_HEIGHT / 2 - (2 * INTERNAL_TILE_OFFSET) - (4 * INTERNAL_SQUARE_SIZE));
+  let b = canvasPositionToMapPosition(a, pixel_offset, CANVAS_WIDTH, CANVAS_HEIGHT);
+  expect(b.getX()).toBe(tile_corner.getX());
+  expect(b.getY()).toBe(tile_corner.getY());
+});
+
+test("tile -1,4 map_position reversible translation", () => {
+  let pixel_offset = 0;
+  let tile_corner = new MapPosition();
+  tile_corner.setX(-1);
+  tile_corner.setY(4);
+  let a = mapPositionToCanvasPosition(tile_corner, pixel_offset, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let b = canvasPositionToMapPosition(a, pixel_offset, CANVAS_WIDTH, CANVAS_HEIGHT);
+  expect(b.getX()).toBe(tile_corner.getX());
+  expect(b.getY()).toBe(tile_corner.getY());
+});
+
+test("tile 4,-16 map_position reversible translation", () => {
+  let pixel_offset = 0;
+  let tile_corner = new MapPosition();
+  tile_corner.setX(4);
+  tile_corner.setY(-16);
+  let a = mapPositionToCanvasPosition(tile_corner, pixel_offset, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let b = canvasPositionToMapPosition(a, pixel_offset, CANVAS_WIDTH, CANVAS_HEIGHT);
+  expect(b.getX()).toBe(tile_corner.getX());
+  expect(b.getY()).toBe(tile_corner.getY());
+});
+
+// test("heister 0,0 reversible translation", () => {
+//   let pixel_offset = 0;
+//   let center = new MapPosition();
+//   center.setX(0);
+//   center.setY(0);
+//   let a = heisterMapPositionToCanvasPosition(center, pixel_offset, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+//   let b = heisterCanvasPositionToMapPosition(a, pixel_offset, CANVAS_WIDTH, CANVAS_HEIGHT);
+//   // Technically, as long as I can add the correct internal wall offset + heister square offset, for
+//   // this simple test case, these checks may be useful.
+//   // expect(a.x).toBe(CANVAS_WIDTH / 2);
+//   // expect(a.y).toBe(CANVAS_HEIGHT / 2);
+//   expect(b.getX()).toBe(center.getX());
+//   expect(b.getY()).toBe(center.getY());
+// })
 
 test("map position translation in both directions at non-center should work, too", () => {
   // NOTE: TODO: This fails at big numbers (251 and 252).
@@ -22,8 +89,8 @@ test("map position translation in both directions at non-center should work, too
   var mp = new MapPosition();
   mp.setX(1);
   mp.setY(2);
-  var a = mapPositionToCanvasPosition(mp, pixel_offset, 0, 0);
-  var b = canvasPositionToMapPosition(a, pixel_offset);
+  var a = mapPositionToCanvasPosition(mp, pixel_offset, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  var b = canvasPositionToMapPosition(a, pixel_offset, CANVAS_WIDTH, CANVAS_HEIGHT);
   expect(b.getX()).toBe(mp.getX());
   expect(b.getY()).toBe(mp.getY());
 });
