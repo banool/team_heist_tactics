db191a88ddb9f9479cc556a7bffa138f12952601
Daniel Porteous <danielporteous1@gmail.com>
2020-06-28 08:18:39 -0700

    Aggressively improve frontend performance

--- ui/generate_types.sh
+++ ui/generate_types.sh
@@ -4,8 +4,6 @@ export PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 # Directory to write generated code to (.js and .d.ts files)
 export OUT_DIR="./src/generated"
 
-ls $PROTOC_GEN_TS_PATH
-
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
