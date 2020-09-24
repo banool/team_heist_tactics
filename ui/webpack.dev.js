b677d923a4f0873f682a6597387f02913f917fa6
Daniel Porteous <danielporteous1@gmail.com>
2020-09-24 12:34:48 -0700

    [ui] Fix build since webpack upgrade

--- ui/webpack.dev.js
+++ ui/webpack.dev.js
@@ -1,6 +1,6 @@
-const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
+const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
   devtool: "eval-source-map",
